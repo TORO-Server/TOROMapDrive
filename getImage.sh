1a5ef9f2b36f09a1941fa0727ffe35948aefad69
Marumasa <77374813+malken21@users.noreply.github.com>
2025-04-24 22:17:12 +0900

    Update getImage.sh

--- getImage.sh
+++ getImage.sh
@@ -8,8 +8,8 @@ TIME=$(date "+%Y-%m-%d")
 # Dynmapの画像を取得する
 # https://github.com/hampta/minecraft-dynmap-timemachine
 cd ./minecraft-dynmap-timemachine
-python3 dynmap-timemachine.py -v https://map.torosaba.net/ main flat [0,100,-2000] [25,25] 4 ../map/main-flat-origin -th 64
-python3 dynmap-timemachine.py -v https://map.torosaba.net/ flat flat [0,100,0] [10,10] 4 ../map/flat-flat-origin -th 64
+python3 dynmap-timemachine.py -v https://map.torosaba.net/ main flat [0,100,-2000] [25,25] 4 ../map/main-flat-origin -th 32
+python3 dynmap-timemachine.py -v https://map.torosaba.net/ flat flat [0,100,0] [10,10] 4 ../map/flat-flat-origin -th 32
 
 # ffmpeg を利用して 解像度を下げたpngファイルを生成
 cd ../map
